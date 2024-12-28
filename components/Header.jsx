d9fa3e7273d9c281833bc722a89258884cdb3a2e
abtargus7 <abtargus7@gmail.com>
2024-12-28 14:12:12 +0530

    home page complete

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,31 +4,34 @@ import { Button } from './ui/button'
 
 //components
 import Nav from './Nav'
+import MobileNav from './MobileNav'
 
 
 const Header = () => {
   return (
-    <header className='py-8 xl:py-12 text-white'> 
-        <div className="container mx-auto flex justify-between items-center">
-          {/* logo */}
-            <Link href="/">
-              <h1 className='text-4xl font-semibold '>
-                Abhi<span className='text-accent'>.</span>
-              </h1>
-            </Link>
+    <header className='py-8 xl:py-12 text-white'>
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className='text-4xl font-semibold '>
+            Abhi<span className='text-accent'>.</span>
+          </h1>
+        </Link>
 
-            {/* Desktop Nav and hire me button*/}
-            <div className='hidden xl:flex items-center gap-8'>
-              <Nav />
-              <Link href={'/contact'}>
-                <Button>Hire Me</Button>
-              </Link>
-            </div>
-
-            {/* Mobile Nav */}
-            <div className="xl:hidden">mobile nav</div> 
-            
+        {/* Desktop Nav and hire me button*/}
+        <div className='hidden xl:flex items-center gap-8'>
+          <Nav />
+          <Link href={'/contact'}>
+            <Button>Hire Me</Button>
+          </Link>
         </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+
+      </div>
     </header>
   )
 }
