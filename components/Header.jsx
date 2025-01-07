f7e37f8783a4b646cf4f359b48f43f26a42e14f1
abtargus7 <abtargus7@gmail.com>
2025-01-07 16:32:50 +0530

    ready to host

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
           <h1 className='text-4xl font-semibold '>
-            Abhi<span className='text-accent'>.</span>
+          <span className='text-accent'>abhi</span>Stack<span className='text-accent'>.</span>io
           </h1>
         </Link>
 
