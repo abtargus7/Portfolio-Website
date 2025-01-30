8e464aa552ff7cb7faf80018a91c297e31dc470b
abtargus7 <abtargus7@gmail.com>
2025-01-30 22:47:45 +0530

    add social links

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 import {FaGithub, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa"
 
 const socials = [
-  {icon: <FaGithub />, path: ""},
-  {icon:  <FaLinkedin />, path: ""},
-  {icon: <FaTwitter />, path: ""},
+  {icon: <FaGithub />, path: "https://github.com/abtargus7"},
+  {icon:  <FaLinkedin />, path: "https://www.linkedin.com/in/abhishek-pawara/"},
+  {icon: <FaTwitter />, path: "https://x.com/PawaraAbhijeet"},
   {icon: <FaYoutube />, path: ""},
 ]
 
